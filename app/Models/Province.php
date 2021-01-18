7d5a5af2471f9e4765d143eba840bc273bd71ddd
jerson <jerson.baltazar@myxlog.com>
2021-01-18 14:36:02 +0800

    Initialized relationships of address & Added laravel passport for auth

--- app/Models/Province.php
+++ app/Models/Province.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Province extends Model
 {
     use HasFactory;
+
+    public function cities()
+    {
+        return $this->hasMany(City::class, 'provCode', 'provCode');
+    }
+
+    public function region()
+    {
+        return $this->belongsTo(Region::class, 'regCode', 'regCode');
+    }
 }
--- app/Models/Region.php
+++ app/Models/Region.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Region extends Model
 {
     use HasFactory;
+
+    public function provinces()
+    {
+        return $this->hasMany(Province::class, 'regCode', 'regCode');
+    }
 }
