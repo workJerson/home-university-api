fb837976a3c9af44454c4f881b9c3936d523bab7
jerson <jerson.baltazar@myxlog.com>
2021-04-05 21:18:01 +0800

    Adjustment on email template on enrollment

--- resources/views/mail/enrollee/new.blade.php
+++ resources/views/mail/enrollee/new.blade.php
@@ -1,10 +1,10 @@
 @extends('mail.template')
 @section('content')
     <p>Hello <span style="font-weight: bold">{{$name ?? 'test-name'}}!</span></p>
-    <p>You have successfully submitted your application to Home University.</p>
+    <p>You have successfully submitted your application to U-ED.</p>
     <p>We will process your application and we'll get back to you once we completed the validation.</p>
     <p>Thank you!</p>
-    <p>Home University Team</p>
+    <p>U-ED Team</p>
 @endsection
 
 
--- resources/views/mail/template.blade.php
+++ resources/views/mail/template.blade.php
@@ -14,7 +14,7 @@
                 <table align='left' border='0' cellpadding='0' cellspacing='0' width='70' style=' width: 100%;'>
                    <tbody>
                       <tr>
-                         <td height='120' style='padding: 0 0 0 40px;'><img src='https://home-university.com/assets/icons/HU.png' style='width: 70px; height: 70px; max-width: none;' /></td>
+                         <td height='120' style='padding: 0 0 0 40px;'><img src='https://portal.u-ed.net/assets/icons/UED.png' style='width: 70px; height: 70px; max-width: none;' /></td>
                       </tr>
                    </tbody>
                 </table>
@@ -49,9 +49,9 @@
                             <table border='0' cellpadding='0' cellspacing='0' style='border-collapse: collapse; font-family: Helvetica, Arial, sans-serif; font-weight: 300;' width='100%'>
                                <tbody>
                                   <tr>
-                                     <td align='left' colspan='2' style='color: #646465; font-size: 11px;'><span style=' font-weight: bold; font-size: 15px;'>Home University</span><br />Room 101, Philippine Social Science Council,<br />Inc. PSSC Center, Commonwealth Avenue Diliman,<br />Quezon City</td>
-                                     <td align='center' colspan='2' style='color: #646465; font-size: 11px;'><a target='_blank' href='https://home-university.com' style='color: #646465; text-decoration: none; font-size: 12px; text-decoration: underline;'>www.home-university.com</a></td>
-                                     <td align='right' rowspan='2' style='padding-left: 30px; color: #646465; font-size: 11px;'><a href='mailto:homeuniversity2021@gmail.com' style='color: #646465; text-decoration: none; text-decoration: underline;'>homeuniversity2021@gmail.com</a><br /><a href='mailto:homeuniversity2021@yahoo.com' style='color: #646465; text-decoration: none; text-decoration: underline;'>homeuniversity2021@yahoo.com</a></td>
+                                     <td align='left' colspan='2' style='color: #646465; font-size: 11px;'><span style=' font-weight: bold; font-size: 15px;'>U-ED</span><br />Room 101, Philippine Social Science Council,<br />Inc. PSSC Center, Commonwealth Avenue Diliman,<br />Quezon City</td>
+                                     <td align='center' colspan='2' style='color: #646465; font-size: 11px;'><a target='_blank' href='https://portal.u-ed.net' style='color: #646465; text-decoration: none; font-size: 12px; text-decoration: underline;'>www.portal.u-ed.net</a></td>
+                                     <td align='right' rowspan='2' style='padding-left: 30px; color: #646465; font-size: 11px;'><a href='mailto:u.ed.portal@gmail.com' style='color: #646465; text-decoration: none; text-decoration: underline;'>u.ed.portal@gmail.com</a><br /><a href='mailto:u.ed.portal@yahoo.com' style='color: #646465; text-decoration: none; text-decoration: underline;'>u.ed.portal@yahoo.com</a></td>
                                   </tr>
                                </tbody>
                             </table>
