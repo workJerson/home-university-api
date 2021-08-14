776a380cbb6a812fa02c7ad1d85cad64a24e00fd
jerson <jerson.baltazar@myxlog.com>
2021-08-14 17:22:16 +0800

    Change default landing page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    // dd(config('app.url'));
-
-    return view('mail.user.reset-password');
+    return view('welcome');
 });
